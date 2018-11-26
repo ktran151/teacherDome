218fb6fd837f3c4f85a97f5b22847e4f4eec4725
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-11-26 10:21:15 -0500

    inital commit

---
+++ src/main/java/org/wecancodeit/teacherDome/TeacherDomeApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.teacherDome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherDomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherDomeApplication.class, args);
+	}
+}
